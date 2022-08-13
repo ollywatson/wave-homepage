afcac18609e1c836b2a9b2031fe12d17fe752043
Olly Watson <olly.watson@deliveroo.co.uk>
2022-08-13 23:58:27 +0100

    Improved responsive layout on vertical broswer resizing

--- components/Hero.js
+++ components/Hero.js
@@ -6,26 +6,28 @@ function Hero() {
         <>
         <div className="background"></div>
         <div className="background-texture"></div>
-        <Container className="container-top">
-            <Row>
-                <Col xs={12} md={7} lg={7} xl={{ span: 6, offset: 1 }} xxl={{ span: 6, offset: 1 }}>
-                    <div className="content-outer">
-                        <div className="content-inner">
-                            <div className="content-headers">
-                                <h1>Let<span className="apostrophe">’</span>s talk</h1>
-                                <p>Wave is a voice-first messaging app that helps you communicate with the ones you love</p>
+        <div className="main-container">
+            <Container className="container-top">
+                <Row>
+                    <Col xs={12} md={7} lg={7} xl={{ span: 6, offset: 1 }} xxl={{ span: 6, offset: 1 }}>
+                        <div className="content-outer">
+                            <div className="content-inner">
+                                <div className="content-headers">
+                                    <h1>Let<span className="apostrophe">’</span>s talk</h1>
+                                    <p>Wave is a voice-first messaging app that helps you communicate with the people you love</p>
+                                </div>
+                                <a href="https://apple.co/3JTxuvA"><img className="appstore-download" src="assets/appstore-button.svg"></img></a>
                             </div>
-                            <a href="https://apple.co/3JTxuvA"><img className="appstore-download" src="assets/appstore-button.svg"></img></a>
                         </div>
-                    </div>
-                </Col>
-                <Col xs={{ span: 10, offset: 1 }} md={{ span: 5, offset: 0}} lg={4} xl={3} xxl={4}>
-                    <div className="image-padder">
-                        <img className="hero-image" src="/assets/iphone-ui.png"></img>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+                    </Col>
+                    <Col xs={{ span: 10, offset: 1 }} md={{ span: 5, offset: 0}} lg={4} xl={3} xxl={4}>
+                        <div className="image-padder">
+                            <img className="hero-image" src="/assets/iphone-ui.png"></img>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
         </>
     );
 }
