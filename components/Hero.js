2fc5cf5d6b2b2d6217ee860dd3faa805543cc429
Olly Watson <olly.watson@deliveroo.co.uk>
2022-08-14 18:14:29 +0100

    Design fixes

--- components/Hero.js
+++ components/Hero.js
@@ -14,7 +14,7 @@ function Hero() {
                             <div className="content-inner">
                                 <div className="content-headers">
                                     <h1>Let<span className="apostrophe">’</span>s talk</h1>
-                                    <p>Wave is a voice-based messaging app for the conversations you care about.</p>
+                                    <p>Wave is a voice-based messaging app for conversations you care about.</p>
                                 </div>
                                 <a className="appstore-download" href=""><img src="assets/appstore-button.svg"></img></a>
                             </div>
